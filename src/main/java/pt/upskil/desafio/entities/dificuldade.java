593c1344861391e7964ff5c8f1fb0a5c18d6893a
pedro.mda@gmail.com <Molly/2020>
2021-03-26 15:51:30 +0000

    entities

--- src/main/java/pt/upskil/desafio/entities/dificuldade.java
+++ src/main/java/pt/upskil/desafio/entities/dificuldade.java
@@ -1,4 +1,20 @@
 package pt.upskil.desafio.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class dificuldade {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
 }
--- src/main/java/pt/upskil/desafio/entities/pergunta.java
+++ src/main/java/pt/upskil/desafio/entities/pergunta.java
@@ -1,4 +1,19 @@
 package pt.upskil.desafio.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class pergunta {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }
