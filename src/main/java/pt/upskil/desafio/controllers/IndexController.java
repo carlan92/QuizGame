80972e1d41eec2e01052b48e32af37583c9e0c5a
pedro.mda@gmail.com <Molly/2020>
2021-03-26 15:16:49 +0000

    index

---
+++ src/main/java/pt/upskil/desafio/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package pt.upskil.desafio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
+    }
+}
