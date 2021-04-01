b6323d64d65f7012bfb718f262272a746ee53c9f
pedro.mda@gmail.com <Molly/2020>
2021-04-01 18:12:06 +0100

    dificuldade precisão

--- src/main/java/pt/upskil/desafio/entities/Dificuldade.java
+++ src/main/java/pt/upskil/desafio/entities/Dificuldade.java
@@ -6,10 +6,10 @@ import java.time.Duration;
 
 @Getter
 public enum Dificuldade {
-    FACIL("fácil", "Fácil", 1, Duration.ofSeconds(20), 50),
-    MEDIA("média","Média", 2, Duration.ofSeconds(30), 75),
-    DIFICIL("difícil","Difícil", 3, Duration.ofSeconds(40), 100),
-    IMPOSSIVEL("impossível","Impossível", 4, Duration.ofSeconds(60), 200);
+    FACIL("fácil", "Fácil", 1, Duration.ofSeconds(20), 50, 0.7),
+    MEDIA("média","Média", 2, Duration.ofSeconds(30), 75, 0.5),
+    DIFICIL("difícil","Difícil", 3, Duration.ofSeconds(40), 100, 0.3),
+    IMPOSSIVEL("impossível","Impossível", 4, Duration.ofSeconds(60), 200, 0.2);
 
 
     private final String apiText;
@@ -17,13 +17,20 @@ public enum Dificuldade {
     private final int type;
     private final Duration duration;
     private final int pontos;
+    private final double precisaoAjuda;
 
-    Dificuldade(String apiText, String buttonText, int type, Duration duration, int pontos) {
+    Dificuldade(String apiText,
+                String buttonText,
+                int type,
+                Duration duration,
+                int pontos,
+                double precisaoAjuda) {
         this.apiText = apiText;
         this.buttonText = buttonText;
         this.type = type;
         this.duration = duration;
         this.pontos = pontos;
+        this.precisaoAjuda = precisaoAjuda;
     }
 
     public static Dificuldade getDificuldadeFromApiText(String apiText) {
