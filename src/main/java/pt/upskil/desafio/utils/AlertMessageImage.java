1c2cb76873fc7f3d5ba2f1be48c663c6778eda07
carlan92 <68751305+carlan92@users.noreply.github.com>
2021-04-04 11:33:37 +0100

    bootstrap cleaning

--- src/main/java/pt/upskil/desafio/utils/AlertMessageImage.java
+++ src/main/java/pt/upskil/desafio/utils/AlertMessageImage.java
@@ -2,7 +2,7 @@ package pt.upskil.desafio.utils;
 
 public enum AlertMessageImage {
     // Alert message image
-    FAILURE("/imagens/minion-error.png", "/imagens/minions-error-audio1.mp3"),
+    FAILURE("/imagens/img-error.png", "/imagens/minions-error-audio1.mp3"),
     SUCCESS("/imagens/minion_success.png", "/imagens/minions-banana-song.mp3");
 
     private final String imageURL;
